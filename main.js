793e6a9c7c8b679ad91d4cb2aebee40849470811
DaniloFreze <danilo.santana@escola.pr.gov.br>
2024-05-15 21:02:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-let nomeUsuário = prompt("Qual é o seu nome?");
+let nomeUsuário = ""
 let elemento = document.querySelector("#nome-usuário"); 
-elemento.textContent = nomeUsuário;
 
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Qual é o seu nome?");
+    }
+
+    if(nomeUsuário == null){
+        elemento.textContent = "Seja muito bem-vindo!"; 
+    }else{
+        elemento.textContent = nomeUsuário 
+    }
